f2b5521ee682a3f30c0276237409c0ac6cc7cbdf
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-03-01 11:01:33 -0500

    Added ability to place X on board.

--- src/Game.js
+++ src/Game.js
@@ -5,15 +5,18 @@ import Board from './Board';
 export default class Game extends Component {
     render() {
         return (
-        <div className="game">
-            <div className="game-board">
-                <Board />
+            <div>
+                <h1>Tic Tac Toe</h1>
+                <div className="game">
+                    <div className="game-board">
+                        <Board />
+                    </div>
+                    <div className="game-info">
+                        <div>{/* status */}</div>
+                        <ol>{/* TODO */}</ol>
+                    </div>
+                </div>
             </div>
-            <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
-            </div>
-        </div>
         );
     }
 }
